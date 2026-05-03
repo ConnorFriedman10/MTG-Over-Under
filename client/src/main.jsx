30d8088216a6bb69021685582123862384672ea4
Connor Friedman <friedman.connor99@gmail.com>
2026-05-03 01:20:37 -0400

    added main page and react routing
    added main page for scoreboard+mode selection, will work on tmrw

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import IntroPage from './IntroPage.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <IntroPage />
+    </BrowserRouter>
   </StrictMode>,
 )
